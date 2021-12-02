26d1155d3fc56422fa086756b09d6c1095d1e7b6
Italo Gabriel <igbehh@gmail.com>
2021-12-02 09:21:50 -0300

    att

--- lib/modules/home/presenter/home_screen.dart
+++ lib/modules/home/presenter/home_screen.dart
@@ -5,7 +5,6 @@ import '../domain/entities/todo.dart';
 import 'components/todo_card.dart';
 import 'home_controller.dart';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -26,7 +25,7 @@ class _HomeScreenState extends ModularState<HomeScreen, HomeController> {
         title: const Text("To-do App"),
         centerTitle: true,
       ),
-      body: Obx(() => _listOfTodo() ),
+      body: Obx(() => _listOfTodo()),
       floatingActionButton: FloatingActionButton(
         onPressed: () => controller.onActionTap(context),
         child: Text("Add"),
@@ -34,20 +33,24 @@ class _HomeScreenState extends ModularState<HomeScreen, HomeController> {
     );
   }
 
-  Widget _listOfTodo(){
+  Widget _listOfTodo() {
     return controller.isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: controller.todoList.length,
-              itemBuilder: (context, index) {
-                Todo todo = controller.todoList[index];
-                return TodoCardComponent(
-                  todo: todo,
-                  removeTap: () => controller.removeById(todo.id!),
-                  editTap: () => controller.onEditTap(context, todo),
-                );
-              });
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : controller.todoList.isEmpty
+            ? Center(
+                child: Text("Empty List"),
+              )
+            : ListView.builder(
+                itemCount: controller.todoList.length,
+                itemBuilder: (context, index) {
+                  Todo todo = controller.todoList[index];
+                  return TodoCardComponent(
+                    todo: todo,
+                    removeTap: () => controller.removeById(todo.id!),
+                    editTap: () => controller.onEditTap(context, todo),
+                  );
+                });
   }
 }
